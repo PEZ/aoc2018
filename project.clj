b9102d072c05b5e9e7bb965c311f0864fce3c308
Hoppy <hoppy@gnurdle.com>
2019-06-24 06:19:31 -0500

    cleanout hardwired cljs repl crud so we can test using calva custom repl

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject aoc2018 "0.1.0-SNAPSHOT"
   :description "Advent of Code in CLJS"
-  :url "//http://paradoxescaperooms.com"
+  :url "http://gnurdle.com.com"
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.reader "1.3.2"]
-                 [cljs-node-io "1.1.2"]
-                 [figwheel-sidecar "0.5.17"]
-                 [cider/piggieback "0.3.10"]]
+                 [cljs-node-io "1.1.2"]]
 
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
 
@@ -18,11 +16,9 @@
             [lein-cljsbuild "1.1.7"]
             [lein-ancient "0.6.15"]]
 
-  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
-
   :cljsbuild
   {:builds
-   [{:id "aoc"
+   [{:id "dev"
      :source-paths ["src"]
      :compiler {:main aoc.core
                 :verbose false
@@ -33,4 +29,13 @@
                 :optimizations :none
                 :cache-analysis true
                 :parallel-build true}}]})
+
+;; Calva custom cljs repl snippet:
+;;
+;;    "calva.customCljsRepl": {
+;;        "name" : "Node",
+;;        "startCode" : "(do (require '[cljs.repl.node]) (require '[cider.piggieback]) (cider.piggieback/cljs-repl (cljs.repl.node/repl-env)))",
+;;        "connectedRegExp": "To quit, type: :cljs/quit"
+;;    }
+
 
