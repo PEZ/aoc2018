0fa9e8603d9254313619ba633aa9bd6bac932954
Peter Strömberg <pez@pezius.com>
2019-06-24 13:38:20 +0200

    Add custom cljs repl settings. Remove tools.nrep

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
-                 [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.reader "1.3.2"]
                  [cljs-node-io "1.1.2"]]
 
@@ -25,7 +24,7 @@
                 :output-dir "bin"
                 :output-to "bin/aoc.js"
                 :target :nodejs
-                :source-map true 
+                :source-map true
                 :optimizations :none
                 :cache-analysis true
                 :parallel-build true}}]})
